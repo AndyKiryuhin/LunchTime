6eb8f33b116fc82ceb7a6a238529c3b4ce7375e0
unknown <ankiryuhin@DC2-IT-324-03.rttv.ru>
2015-09-07 12:56:28 +0300

    Add botan.io

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var TelegramBot = require('node-telegram-bot-api');
 // var token = '120468813:AAHeg2aGmnzXdzeRIem-TBjnCTkX9HHxkVM';
 var botantoken = '80701';
-var botan = require('botanio')(botantoken);
+
 
 
 var token = '120565725:AAEalgLPaVIHFSYngYnOO6-W-pTwvrRNXwI';
@@ -59,6 +59,7 @@ var PutinStickerArray = ["BQADAgADuQMAAjq5FQJZfuBnaRaTbwI",
 
 var bot = new TelegramBot(token, {polling: true});
 
+
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
 	var randSticker = PutinStickerArray[Math.floor(Math.random() * PutinStickerArray.length)];
@@ -66,7 +67,8 @@ bot.on('message', function (msg) {
     console.log(msg);
 	//var myString = "Сегодня выиграл ресторан "+randRest;
 	// myString = JSON.parse( JSON.stringify( myString ) )
-	botan.track(message, 'Start');
+	var botan = require('botanio')(botantoken);
+	botan.track(msg, 'Start');
     bot.sendSticker(chatId, randSticker);
 	bot.sendMessage(chatId, "РЎРµРіРѕРґРЅСЏ РїСЂРµРґР»Р°РіР°РµС‚СЃСЏ РїРѕСЃРµС‚РёС‚СЊ "+randRest , {caption: "This is Dalet Bot from RT"});
 	// bot.sendMessage(chatId, msg.text , {caption: "This is Dalet Bot from RT"});
